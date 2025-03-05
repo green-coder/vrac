9c558bf0de52764a92be5cf3f9a9a229e67a6085
Vincent Cantin <vincent@404.taipei>
2025-03-05 17:36:43 +0200

    fix up

--- example/test-app/src/example/reactive_fragment/core.cljs
+++ example/test-app/src/example/reactive_fragment/core.cljs
@@ -5,18 +5,20 @@
 (defn counter-component [counter-state]
   ($ :div
      "Counter value: " counter-state
-     ($ :div ($ :button {:on-click #(swap! counter-state inc)} "Increment"))))
+     ($ :div ($ :button {:on-click #(swap! counter-state inc)} "+1"))
+     ($ :div ($ :button {:on-click #(swap! counter-state (fn [n] (+ n 2)))} "+2"))))
 
 (defn- if-fragment-component []
   (let [counter-state (sr/create-state 0)]
     ($ :article
        ($ :h3 "If fragment")
        ($ counter-component counter-state)
-       (vw/if-fragment (sr/create-memo
+       (vw/if-fragment (sr/create-derived
                          (fn []
-                           (even? @counter-state)))
-                       ($ :div "The value is even.")
-                       ($ :div "The value is odd.")))))
+                           (even? @counter-state))
+                         {:metadata {:name "if-cond"}})
+         ($ :div "The value is even.")
+         ($ :div "The value is odd.")))))
 
 (defn reactive-fragment-root []
   ($ :div
