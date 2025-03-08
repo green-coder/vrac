64b38925652b2be419a7bcb4bcf37164b0eda4bd
Vincent Cantin <vincent@404.taipei>
2025-03-08 09:40:33 +0200

    Improved the DIY context sample, add a comment to explain its limits.

--- example/test-app/src/example/context/core.cljs
+++ example/test-app/src/example/context/core.cljs
@@ -2,29 +2,32 @@
   (:require [signaali.reactive :as sr]
             [vrac.web :as vw :refer [$]]))
 
-(def ^:dynamic *context* (sr/create-signal {:a 1}))
+(def ^:dynamic *diy-context* (sr/create-signal {:a 10}))
+
+(defn- display-diy-context [label]
+  (let [context *diy-context*]
+    ($ :div label ": " context)))
 
 (defn- diy-context-article []
-  (let [counter (sr/create-signal 20)]
+  (let [counter (sr/create-signal 0)]
     ($ :article
-       ($ :h3 "Context")
-
-       ($ :button {:on-click #(swap! counter inc)} counter)
-
-       (let [;; Read the current context, which is expectedly a reactive node.
-             context *context*
-             ;; Create a derived reactive node, for an inner context.
-             inner-context (sr/create-derived (fn [] (-> @context (assoc :b @counter))))]
-         ;; Set it as the new current context within this scope.
-         (binding [*context* inner-context]
-
+       ($ :h3 "DIY context using dynamic variables")
+       ($ :button {:on-click #(swap! counter inc)} "counter = " counter)
+       (display-diy-context "global context")
+       (let [context *diy-context* ;; The current context, expectedly a reactive node.
+             local-context (sr/create-derived (fn [] (-> @context
+                                                         (assoc :counter @counter)
+                                                         (update :a + @counter))))]
+         ;; Create a derived inner context, and set it as the new current one within this scope.
+         (binding [*diy-context* local-context]
            ;; Read the new current context from somewhere in the scope.
-           (let [context *context*]
-             ($ :div "inner context: " context))))
+           (display-diy-context "local context")))
+       (display-diy-context "back to the global context")
 
-       ;; Read the new current context from somewhere outside the scope.
-       (let [context *context*]
-         ($ :div "outer context: " context)))))
+       ($ :div
+          ($ :strong "Warning: ")
+          "This sample won't work if we call the inner components using "
+          ($ :code "($ display-diy-context ,,,)")))))
 
 (defn context-root []
   ($ :div
