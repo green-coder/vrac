638cf6cd179ee4a3f5b792e7704fe441e828a38f
Vincent Cantin <vincent@404.taipei>
2025-03-09 21:38:19 +0200

    small tweaks

--- example/test-app/src/example/svg/core.cljs
+++ example/test-app/src/example/svg/core.cljs
@@ -87,21 +87,23 @@
         ($ :path {:d "M12.79,55.7l3.3,3.3c.65,.65,1.51,1,2.42,1H55.7c1.11,0,2.01-.9,2.01-2.01v-12.96c0-.82-.2-1.63-.58-2.36l-3.43-6.53c-.38-.72-.94-1.35-1.62-1.82l-19.64-13.51V5.69c0-.56-.27-1.08-.73-1.39-.46-.32-1.04-.39-1.56-.19l-10.78,4.09L8.58,4.11c-.52-.2-1.1-.13-1.56,.19s-.73,.84-.73,1.39v14.64c0,.92,.36,1.78,1,2.42l4.49,4.49v26.02c0,.92,.36,1.78,1,2.42Zm1-26.45l3.26,3.26c1.28,1.28,3.36,1.28,4.63,0l5.26-5.26-13.16,23.23V29.25Zm2.65-.18h5.87l-2.03,2.03c-.5,.5-1.31,.5-1.81,0l-2.03-2.03Zm19.15,28.93l20.13-12.19v12.19h-20.13Zm19.77-14.39s0,.05,.02,.07l-5.96,3.61,3.11-9.05,2.83,5.37Zm-4.41-7.63c.08,.05,.11,.14,.18,.2l-4.38,12.72-15.02,9.1h-13.21c-.38,0-.74-.15-1.01-.42l-3.3-3.3c-.07-.07-.06-.19-.11-.28L31.77,22.79l19.17,13.19ZM12.08,15.14l-3.79,3.79V8.47l3.79,6.67Zm-2.55-8.53l8.03,3.05-4.02,4.02-4.02-7.06Zm20.9,1.86v10.46l-3.79-3.79,3.79-6.67Zm-5.26,5.2l-4.02-4.02,8.03-3.05-4.02,7.07Zm-16.47,7.67h0l10.66-10.66,10.66,10.66-5.72,5.72H14.43l-5.72-5.72Z"}))))
 
 (defn- foreign-object-inside-svg []
-  ($ :svg {:xmlns "http://www.w3.org/2000/svg"
-           :width "40rem"
-           :height "40rem"
-           :viewBox "0 0 200 200"}
-     ($ :polygon {:points "5,5 195,10 185,185 10,195"})
-     ($ :foreignObject {:x "20"
-                        :y "20"
-                        :width "160"
-                        :height "160"}
-        ($ :div {:xmlns "http://www.w3.org/1999/xhtml"
-                 :style {:background-color "purple"
-                         :color "pink"
-                         :height "100%"}}
-           "This is an HTML div inside a SVG element. "
-           "It is wrapped inside a 'foreignObject' SVG element."))))
+   ($ :article
+      ($ :h2 "<foreignObject> element inside SVG")
+      ($ :svg {:xmlns "http://www.w3.org/2000/svg"
+               :width "40rem"
+               :height "40rem"
+               :viewBox "0 0 200 200"}
+         ($ :polygon {:points "5,5 195,10 185,185 10,195"})
+         ($ :foreignObject {:x "20"
+                            :y "20"
+                            :width "160"
+                            :height "160"}
+            ($ :div {:xmlns "http://www.w3.org/1999/xhtml"
+                     :style {:background-color "purple"
+                             :color "pink"
+                             :height "100%"}}
+               "This is an HTML div inside a SVG element. "
+               "It is wrapped inside a 'foreignObject' SVG element.")))))
 
 (defn svg-root []
   ($ :div
