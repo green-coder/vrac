ce4c69e94b1c5166e674209529035d5a6f7b96db
Vincent Cantin <vincent@404.taipei>
2025-03-22 08:47:09 +0200

    Moved the playwright directory somewhere else, will keep `/test` for unit test's sources.

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     timeout: 60 * 60 * 1000, // Waiting for shadow-cljs to download its dependencies.
-    command: "cd ../../example/test-app && npm install && npm run release && npm run server",
+    command: "cd ../example/test-app && npm install && npm run release && npm run server",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
